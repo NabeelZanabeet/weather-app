a07ed1457e70a520bb20fc9a4b1305d35ab068eb
NabeelZanabeet <nabeelzanabeet@gmail.com>
2019-03-14 14:26:53 +0200

    Using enzyme to write snapshots

--- __tests__/components-render-test.js
+++ __tests__/components-render-test.js
@@ -2,6 +2,7 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import App from '../src/App';
 import DetailsScreen from '../src/components/DetailsScreen';
 import HomeScreen from '../src/components/HomeScreen';
@@ -10,10 +11,24 @@ it('DetailsScreen renders correctly', () => {
   renderer.create(<DetailsScreen />);
 });
 
+it('DetailsScreen should render', () => {
+  const component = shallow(<DetailsScreen />);
+  expect(component).toMatchSnapshot();
+});
 it('HomeScreen renders correctly', () => {
   renderer.create(<HomeScreen />);
 });
 
+it('HomeScreen should render', () => {
+  const component = shallow(<HomeScreen />);
+  expect(component).toMatchSnapshot();
+});
+
 it('App renders correctly', () => {
   renderer.create(<App />);
+});
+
+it('App should render', () => {
+  const component = shallow(<App />);
+  expect(component).toMatchSnapshot();
 });

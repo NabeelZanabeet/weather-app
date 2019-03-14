fa45ab4eb18d777a3e432c72e990a231843b45db
NabeelZanabeet <nabeelzanabeet@gmail.com>
2019-03-14 14:51:54 +0200

    Developing a FlatList using some mock random data

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import {
-  View, Text, AppRegistry, Button
+  View, Text, AppRegistry, Button, FlatList
 } from 'react-native';
+import mockWeatherList from '../mockData';
 
 const HomeScreen = props => (
   <View>
     <Text>Home Screen</Text>
+    <FlatList
+      data={mockWeatherList}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={
+        ({ item }) => (
+          <View>
+            <Text>{item.cityname}</Text>
+            <Text>{item.temp}</Text>
+          </View>
+        )
+      }
+    />
     <Button
       title="Go to Details"
       onPress={() => props.navigation.navigate('Details')}
---
+++ src/mockData.js
@@ -0,0 +1,27 @@
+export default mockWeatherList = [
+  {
+    id: 1,
+    cityname: 'London',
+    temp: 20
+  },
+  {
+    id: 2,
+    cityname: 'Berlin',
+    temp: 30
+  },
+  {
+    id: 3,
+    cityname: 'Havana',
+    temp: 40
+  },
+  {
+    id: 4,
+    cityname: 'HongKong',
+    temp: 50
+  },
+  {
+    id: 5,
+    cityname: 'Tokyo',
+    temp: 60
+  }
+];

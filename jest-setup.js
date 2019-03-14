cd3999e6af71137b2be5a864f146579413b30fd3
NabeelZanabeet <nabeelzanabeet@gmail.com>
2019-03-14 14:15:54 +0200

    Setting up Enzyme

--- jest-setup.js
+++ jest-setup.js
@@ -1,4 +1,7 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import mocks from 'react-native-jest-mocks';
 
 mocks.initAll();
+Enzyme.configure({ adapter: new Adapter() });
